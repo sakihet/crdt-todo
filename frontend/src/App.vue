4fd9015db55047bca308becbf480ea9b553473de
saki <sakihet@gmail.com>
2023-06-20 23:59:15 +0900

    add todo front

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,68 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import * as Automerge from '@automerge/automerge'
+import { ref } from 'vue'
+
+let doc = Automerge.init()
+doc = Automerge.change(doc, 'add task', d => {
+  d.tasks = []
+  d.tasks.push({ id: '6db3a402-4e8a-4604-a421-55695673f76a', name: 'task 1', done: false })
+  d.tasks.push({ id: '7cc7dd32-53e9-4602-a576-00d6bbf90257', name: 'task 2', done: true })
+})
+console.log(doc)
+
+const newTaskName = ref("")
+const handleCheck = (id: string) => {
+  console.log('check', id)
+  // TODO
+}
+const handleDelete = (id: string) => {
+  console.log('delete', id)
+  // TODO
+}
+const handleSubmit = () => {
+  console.log('submit', newTaskName.value)
+  doc = Automerge.change(doc, 'add task', d => {
+    // TODO
+    d.tasks.push({ id: '95f6b946-790d-4bb3-bf3a-d4fb543c9b39', name: newTaskName.value, done: false })
+  })
+  newTaskName.value = ''
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>CRDT TODO</h1>
+    <div>
+      <form @submit.prevent="handleSubmit">
+        <input
+          type="text"
+          v-model="newTaskName"
+        />
+      </form>
+      <div>
+        <ul v-for="t in doc.tasks">
+          <li>
+            <div>
+              <input
+                type="checkbox"
+                :checked="t.done"
+                @change="handleCheck(t.id)"
+              />
+              <span>
+                {{ t.name }}
+              </span>
+              <span> | </span>
+              <button
+                type="button"
+                @click="handleDelete(t.id)"
+              >x</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>

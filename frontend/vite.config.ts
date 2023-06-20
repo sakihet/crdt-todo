0b4731fd450999160cf956212a852b34d956f9b7
saki <sakihet@gmail.com>
2023-06-20 22:26:49 +0900

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import wasm from "vite-plugin-wasm"
+import topLevelAwait from "vite-plugin-top-level-await"
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    topLevelAwait(),
+    vue(),
+    wasm()
+  ],
+  optimizeDeps: {
+    exclude: ["@automerge/automerge-wasm"]
+  }
 })

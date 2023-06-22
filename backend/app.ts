a28003639d0b75ad0dee7706299340ae569e77bd
saki <sakihet@gmail.com>
2023-06-22 09:08:17 +0900

    update backend/app

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,23 @@
 import * as http from 'http'
+import { Server } from 'socket.io'
 
 const port = 5000
-
-const server = http.createServer(
+const httpServer = http.createServer(
   (req, res) => {
     res.end('hello')
   }
 )
-
-server.listen(port)
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173"
+  }
+})
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('message', (data) => {
+    console.log('got message from client', data)
+    io.emit('updates', data)
+  })
+})
+httpServer.listen(port)
 console.log(`server started: http://localhost:${port}`)
